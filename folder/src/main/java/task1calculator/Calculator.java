666091034a2400fa65af6f56f837ed41037c56d4
JuliaPokitko <pokitkojulia@gmail.com>
2016-11-18 01:44:25 +0200

    added new file

---
+++ folder/src/main/java/task1calculator/Calculator.java
@@ -0,0 +1,66 @@
+package task1calculator;
+
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        int operand1 = 20;
+        int operand2 = 3;
+        int operand = 0;
+        double operand3 = 6.5;
+        String operation = "/";
+
+        if (operation.equals("+")) operand = operand1 + operand2;
+        if (operation.equals("-")) operand = operand1 - operand2;
+        if (operation.equals("*")) operand = operand1 * operand2;
+        if (operation.equals("%")) {
+            operand3 = operand1 % operand2;
+            System.out.println(operand3);
+        }
+
+
+        if (operation.equals("/")){
+            operand3 = operand1 / operand2;
+            System.out.println(operand3);
+        }
+        if (operation.equals("||")) {
+            if (operand1 >= 0) {
+                operand = operand1;
+            } else operand = operand1 * (-1);
+        }
+        System.out.println(operand);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+      /*  int operand3 = operand1 + operand2;
+        System.out.println(operand3);
+
+        operand3 = operand1 - operand2;
+        System.out.println(operand3);
+
+        operand3 = operand1 * operand2;
+        System.out.println(operand3);
+
+        operand3 = operand1 / operand2;
+        System.out.println(operand3);
+
+        if (operand1>=0){
+            operand3 = operand1;
+
+        }else operand3 = operand1 * (-1);
+        System.out.println(operand3);
+
+        double operand4 = operand1 % operand2;
+        System.out.println(operand4); */
+
+
+
