e881cdc6670aed9b5d08967f8a2e970f278eebc0
dreastyd <dreasty.d@gmail.com>
2016-11-18 18:51:09 +0200

    rename folder

---
+++ suhoy/src/main/java/ua/com/hillel/Main.java
@@ -0,0 +1,14 @@
+package ua.com.hillel;
+
+/**
+ * Created by dreasty on 16.11.16.
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    Integer intVar = 11;
+        System.out.println(intVar);
+
+    }
+}
