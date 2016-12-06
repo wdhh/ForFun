118fd5593df0c69cda64c8aab9eb595364a6be13
JuliaPokitko <pokitkojulia@gmail.com>
2016-12-07 00:10:54 +0200

    added new file

---
+++ pokitko/src/main/java/collection/TestCat.java
@@ -0,0 +1,7 @@
+package collection;
+
+/**
+ * Created by Даня on 02.12.2016.
+ */
+public class TestCat {
+}
--- pokitko/src/main/java/task1/Numbers.java
+++ pokitko/src/main/java/task1/Numbers.java
@@ -1,11 +1,31 @@
 package task1;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Numbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//
+        String line = scanner.nextLine();
+        String [] values = line.split(" ");
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < values.length; i++) {
+            int x = Integer.valueOf(values[i]);
+            numbers.add(x);
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 0; j < numbers.size(); j++) {
+              if (j!=i){
+                 if (numbers.get(i) == numbers.get(j)){
+                     System.out.println("Number: " + numbers.get(i));
+                     System.out.println("Index: " + i);
+                     break;
+                 }
+              }
+            }
+        }
     }
 }
