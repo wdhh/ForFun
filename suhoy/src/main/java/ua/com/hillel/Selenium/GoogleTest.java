2eb666f6fbfcbc6c631103ccf1f279c08764b6d0
Viacheslav Sukhyi <vvs@norse.digital>
2016-12-23 13:15:27 +0200

    add selenium test

--- suhoy/src/main/java/ua/com/hillel/Selenium/GoogleTest.java
+++ suhoy/src/main/java/ua/com/hillel/Selenium/GoogleTest.java
@@ -1,6 +1,7 @@
 package ua.com.hillel.Selenium;
 
-import org.apache.wml.WMLEmElement;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,30 +9,89 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by dreasty on 21.12.16.
  */
+
 public class GoogleTest {
     @Test
-    public void googleSearch(){
-        System.setProperty("webdriver.gecko.driver","/home/dreasty/aqa/ForFun/suhoy/src/main/resources/geckodriver");
+    public void googleSearchGeckoDriverFirefox(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
         WebDriver driver = new FirefoxDriver();
-        driver.get("http://google.com/?ncr");
+        driver.get("http://google.com.ua/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Assert.assertTrue(driver.findElement(By.id("body")).isDisplayed());
+        driver.findElement(By.cssSelector("input#lst-ib")).sendKeys("test"+Keys.ENTER);
+//        List<WebElement> result1 = driver.findElements(By.xpath("//div[@class=\"_NId\"]/div[@class=\"srg\"]/div/div/h3/a"));
+        List<WebElement> result = driver.findElements(By.cssSelector("div#ires .g .r a"));
+        Assert.assertEquals(10, result.size());
+        for ( WebElement we: result) {
+            System.out.println(we.getAttribute("href"));
+            System.out.println(we.getText());
+            Assert.assertTrue(we.getAttribute("href").contains("Test") | we.getAttribute("href").contains("test")| we.getText().contains("Test"));
+        }
         driver.quit();
     }
 
     @Test
-    public void yandexSearch(){
-        System.setProperty("webdriver.gecko.driver","/home/dreasty/aqa/ForFun/suhoy/src/main/resources/geckodriver");
+    public void googleSearchChromeDriver(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/chromedriver");
         WebDriver driver = new FirefoxDriver();
-        driver.get("http://yandex.ua");
-        WebElement inputElement = driver.findElement(By.cssSelector("input[id='text']"));
-        Assert.assertTrue(inputElement.isDisplayed());
-        inputElement.sendKeys("Selenium"+Keys.ENTER);
-//        driver.findElement(By.cssSelector(".search2__button button")).click();
-
+        driver.get("http://google.com.ua/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.findElement(By.id("body")).isDisplayed());
+        driver.findElement(By.cssSelector("input#lst-ib")).sendKeys("test"+Keys.ENTER);
+//        List<WebElement> result1 = driver.findElements(By.xpath("//div[@class=\"_NId\"]/div[@class=\"srg\"]/div/div/h3/a"));
+        List<WebElement> result = driver.findElements(By.cssSelector("div#ires .g .r a"));
+        Assert.assertEquals(10, result.size());
+        for ( WebElement we: result) {
+            System.out.println(we.getAttribute("href"));
+            System.out.println(we.getText());
+            Assert.assertTrue(we.getAttribute("href").contains("Test") | we.getAttribute("href").contains("test")| we.getText().contains("Test"));
+        }
         driver.quit();
     }
+    @Test
+    public void yandexSearchGeckoDriverFirefox(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://yandex.ua/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.findElement(By.cssSelector("form.search2")).isDisplayed());
+        driver.findElement(By.cssSelector("input#text")).sendKeys("test"+Keys.ENTER);
+//        List<WebElement> result1 = driver.findElements(By.xpath("//div[@class=\"_NId\"]/div[@class=\"srg\"]/div/div/h3/a"));
+        List<WebElement> result = driver.findElements(By.cssSelector("div.organic a.organic__url"));
+        Assert.assertEquals(10,result.size());
+        for ( WebElement we: result) {
+            System.out.println(we.getAttribute("href"));
+            System.out.println(we.getText());
+            Assert.assertTrue(we.getAttribute("href").contains("Test") | we.getAttribute("href").contains("test")| we.getText().contains("Test")| we.getText().contains("Тест"));
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void yandexSearchChromeDriver(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/chromedriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://yandex.ua/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.findElement(By.cssSelector("form.search2")).isDisplayed());
+        driver.findElement(By.cssSelector("input#text")).sendKeys("test"+Keys.ENTER);
+//        List<WebElement> result1 = driver.findElements(By.xpath("//div[@class=\"_NId\"]/div[@class=\"srg\"]/div/div/h3/a"));
+        List<WebElement> result = driver.findElements(By.cssSelector("div.organic a.organic__url"));
+        Assert.assertEquals(10,result.size());
+        for ( WebElement we: result) {
+            System.out.println(we.getAttribute("href"));
+            System.out.println(we.getText());
+            Assert.assertTrue(we.getAttribute("href").contains("Test") | we.getAttribute("href").contains("test")| we.getText().contains("Test")| we.getText().contains("Тест"));
+        }
+        driver.quit();
+    }
+
 }
